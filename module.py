8a78f96625cb21bd26ff09ba889a674ef052941e
Álvaro Lázaro Gallego <alvaro.lazaro.g@gmail.com>
2016-07-04 12:26:03 +0200

    Test class with DB access

--- module.py
+++ module.py
@@ -1,4 +1,25 @@
+from time import sleep
+
+
 class MyClass:
     @staticmethod
     def sum(a, b):
         return a + b
+
+
+class Api:
+    _users = []
+
+    def get_formatted_users(self):
+        users = self.get_users_from_db()
+        formatted_users = '\n'.join(
+            '{0}: {1}'.format(i + 1, user) for i, user in enumerate(users))
+        return formatted_users
+
+    def create_user(self, user):
+        sleep(1)
+        self._users.append(user)
+
+    def get_users_from_db(self):
+        sleep(1)
+        return self._users
--- test_module.py
+++ test_module.py
@@ -1,6 +1,6 @@
 import unittest
 
-from module import MyClass
+from module import MyClass, Api
 
 
 class MyClassTestCase(unittest.TestCase):
@@ -12,3 +12,12 @@ class MyClassTestCase(unittest.TestCase):
 
     def test_sum_negative(self):
         self.assertEqual(MyClass.sum(-3, -1), -4)
+
+
+class ApiTestCase(unittest.TestCase):
+    def test_formatted_users(self):
+        api = Api()
+        api.create_user('John')
+        formatted_users = api.get_formatted_users()
+        expected_formatted_users = '1: John'
+        self.assertEqual(formatted_users, expected_formatted_users)
